635d85c7e68bef2a0777e1117cfaad8cdfbe2eea
Anna Matviyenko and Alex Zerbach <alex.zerbach@gmail.com>
2016-06-22 20:53:57 -0700

    Add logic for Spec 2

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,11 @@ function vowelCheck(input) {
   vowels.forEach(function(vowel) {
     if (input.charAt(0) === vowel) {
       input = input + "ay";
+    } else if (input !== vowel) {
+      var firstLetter = input.charAt(0);
+      var newInput = input.slice(1);
+      input = newInput + firstLetter + "ay";
+      console.log(input);
     }
   });
   return input;
