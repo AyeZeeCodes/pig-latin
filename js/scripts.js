b0abd8391e1c635e3a6ec429a8a21304d75062b0
Anna Matviyenko and Alex Zerbach <alex.zerbach@gmail.com>
2016-06-22 20:17:56 -0700

    pass first spec and add ay to the end of a word that starts with a vowel

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,21 @@
+var vowels = ["a","e","i","o","u"];
+var newInput;
+
+function vowelCheck(input) {
+  vowels.forEach(function(vowel) {
+    if (input.charAt(0) === vowel) {
+      input = input + "ay";
+    }
+  });
+  return input;
+
+}
+
 $(document).ready(function() {
   $("form#translator").submit(function(event) {
     event.preventDefault();
-
-    var wordInput = $("input#word").val();
-
-    $("#output").text(wordInput).show();
+    var input = $("input#word").val();
+    //var result = vowel(wordInput);
+     $("#output").text(vowelCheck(input));
   });
 });
