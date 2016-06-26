5ca59f439aa8fb175d6c55cc1ea5317f701b2d0e
ayezee33 <ayezee33@gmail.com>
2016-06-26 10:10:38 -0700

    pass second spec

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,20 @@
-var vowels = ["a","e","i","o","u"];
-var newInput;
 
 function vowelCheck(input) {
-  vowels.forEach(function(vowel) {
-    if (input.charAt(0) === vowel) {
-      input = input + "ay";
-    } else if (input !== vowel) {
-      var firstLetter = input.charAt(0);
-      var newInput = input.slice(1);
-      input = newInput + firstLetter + "ay";
-      console.log(input);
-    }
-  });
-  return input;
+  var vowels = ["a","e","i","o","u"];
+  if (($.inArray(input.charAt(0), vowels) === -1) && ($.inArray(input.charAt(1), vowels) !== -1)) {
+    input = input.slice(1) + input.charAt(0);
+  }
+  if ($.inArray(input.charAt(0), vowels) !== -1) {
+    input = input + 'ay';
+  }
 
+    return input;
 }
 
 $(document).ready(function() {
   $("form#translator").submit(function(event) {
     event.preventDefault();
     var input = $("input#word").val();
-    //var result = vowel(wordInput);
      $("#output").text(vowelCheck(input));
   });
 });
