b65cba861380dea9fdde2988c3efba55c045b1d9
ayezee33 <ayezee33@gmail.com>
2016-06-26 19:53:45 -0700

    Chris' dope logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,38 @@
-
-function vowelCheck(input) {
-  var vowels = ["a","e","i","o","u"];
-  if (($.inArray(input.charAt(0), vowels) === -1) && ($.inArray(input.charAt(1), vowels) !== -1)) {
-    input = input.slice(1) + input.charAt(0);
-  }
-  if ($.inArray(input.charAt(0), vowels) !== -1) {
-    input = input + 'ay';
-  }
-
-    return input;
-}
-
 $(document).ready(function() {
   $("form#translator").submit(function(event) {
     event.preventDefault();
     var input = $("input#word").val();
-     $("#output").text(vowelCheck(input));
+     $("#output").text(wordCheck(input));
   });
 });
+
+var vowels = ["a","e","i","o","u"];
+
+function wordCheck(input) {
+  var words = input.split(" ");
+  var output = ""
+  for (var i = 0; i < words.length; i++) {
+    output += vowelCheck(words[i]) + ' ';
+  }
+  return output;
+}
+
+function vowelCheck(input) {
+  var pos = firstVowel(input);
+  if (pos) {
+    input = input.slice(pos) + input.slice(0, pos);
+  }
+  if ($.inArray(input.charAt(0), vowels) !== -1) {
+    input = input + 'ay';
+  }
+  return input;
+}
+
+function firstVowel(input) {
+  for (var i = 0; i < input.length; i++) {
+    if ($.inArray(input.charAt(i), vowels) !== -1) {
+      return i;
+    }
+  }
+  return false;
+}
